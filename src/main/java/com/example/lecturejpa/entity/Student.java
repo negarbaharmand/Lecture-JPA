7b15688051ed76a34e0cf19628e9e45f6edd5c55
Negar Baharmand <negarbaharmand@outlook.com>
2023-10-17 18:30:31 +0200

    Implement Student entity and configure the entity manager

---
+++ src/main/java/com/example/lecturejpa/entity/Student.java
@@ -0,0 +1,47 @@
+package com.example.lecturejpa.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+
+@Entity
+public class Student {
+
+    // For generating Integer ID: @GeneratedValue(strategy = GenerationType.IDENTITY)
+    //All the annotations should be imported from Jakarta persistence not the spring framework
+    @Id
+    @GeneratedValue(generator = "UUID")
+    @GenericGenerator(name = "UUID", strategy = "org.hibernate.id.UUIDGenerator")
+    private String id;
+
+    @Column(nullable = false, length = 100)
+    @Setter
+    private String firstName;
+    @Column(nullable = false, length = 100)
+    @Setter
+    private String lastName;
+
+    @Column(nullable = false, unique = true)
+    @Setter
+    private String email;
+
+    private boolean status;
+    private LocalDateTime createDate;
+
+
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.status = true;
+        this.createDate = LocalDateTime.now();
+    }
+
+}
